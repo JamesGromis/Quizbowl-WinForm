888c06c038f9a5a6092a7799c3b69d406be793b8
JamesGromis <39245125+JamesGromis@users.noreply.github.com>
2018-11-25 15:15:37 +0100

    Update Form2.Designer.cs
    Form Transition Fix

--- WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -68,7 +68,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(232, 76);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Close this window to Start Quizbowl!";
+            this.button1.Text = "Click Button to begin the Quiz!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
